14027018bf5f7e9c6c6457e2fc26656e5189c582
Kc <40486366+cpwillis@users.noreply.github.com>
2024-04-14 19:33:58 +1000

    shark

---
+++ pull-shark-automation.py
@@ -0,0 +1,38 @@
+import os
+
+
+def update_status_md():
+    try:
+        with open("status.md", "r") as f:
+            text = f.readline()
+            num = [int(x) for x in text.split() if x.isdigit()][0]
+            num += 1
+
+        if num <= 1024:
+            with open("status.md", "w") as f:
+                if num in range(16):
+                    img = "![pull-shark](images/pull-shark-default.png)"
+                elif num in range(128):
+                    img = "![pull-shark](images/pull-shark-bronze.png)"
+                elif num in range(1024):
+                    img = "![pull-shark](images/pull-shark-silver.png)"
+                else:
+                    img = "![pull-shark](images/pull-shark-gold.png)"
+
+                f.write(f"{num} pull requests merged")
+                f.write("<br>")
+                f.write("Currently:")
+                f.write("<br>")
+                f.write(img)
+    except Exception as e:
+        print(f"Error: {e}")
+        with open("status.md", "w") as f:
+            f.write("1 pull request merged")
+            f.write("<br>")
+            f.write("Currently:")
+            f.write("<br>")
+            f.write("![pull-shark](images/pull-shark-default.png)")
+
+
+if __name__ == "__main__":
+    update_status_md()
